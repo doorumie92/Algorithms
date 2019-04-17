6be6d7ba9b148508e48a1dc2d08f74a51869d129
junyoung <doorumiee@gmail.com>
2019-04-17 18:23:20 +0900

    19_04_17

---
+++ Backjoon/Baekjoon/3036.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int Num;
+	cin >> Num;
+	int j = 1;
+	int arr[100] = { 0 };
+
+	for (int i = 0; i < Num; i++)
+	{
+		cin >> arr[i];
+	}
+	for (int i = 1; i < Num; i++)
+	{
+		for (j = arr[0]; j > 0; j--)
+		{
+			if (arr[0] % j == 0 && arr[i] % j == 0)
+			{
+				cout << arr[0] / j << "/" << arr[i] / j << endl;
+				break;
+			}
+		}
+	}
+
+	system("pause");
+}
