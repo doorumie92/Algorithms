2d2e8cb2a033425a77dd568795afc04c19bd58bf
doorumie92 <doorumiee@gmail.com>
2019-05-08 16:03:18 +0900

    1152 not yet solve

---
+++ Backjoon/Baekjoon/1152.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char string[1000000]={'\0',};
+    int count=0;
+    cin.getline(string,1000000,'\n');
+
+    int i=0;
+    for(i=0;i<1000000;i++)
+    {
+        if(string[i] != ' ' && (string[i+1]==' '||string[i+1]=='\0'))
+            count++;
+        if(string[i]=='\0')
+            break;
+    }
+    cout << i << endl;
+    cout <<count-1 << endl;
+    return 0;
+}
