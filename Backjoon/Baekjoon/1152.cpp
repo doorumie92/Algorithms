ab67689fd0bbe2bb2484d19f1056aeac352a6ac6
doorumie92 <doorumiee@gmail.com>
2019-05-15 14:23:42 +0900

    1152.cpp

--- Backjoon/Baekjoon/1152.cpp
+++ Backjoon/Baekjoon/1152.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int main()
 {
-    char string[1000000]={'\0',};
+    char string[1000002]={'\0',};
     int count=0;
-    cin.getline(string,1000000,'\n');
+    cin.getline(string,1000002,'\n');
 
     int i=0;
-    for(i=0;i<1000000;i++)
+    for(i=0;i<1000002;i++)
     {
         if(string[i] != ' ' && (string[i+1]==' '||string[i+1]=='\0'))
             count++;
